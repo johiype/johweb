8ce0e36017e079bf7b471276ec45ce9e0f227a89
Johith Iype <johith.iype@yahoo.com>
2024-07-15 07:57:24 -0400

    Updated Ansible Playbook for bastion host

--- ansible/dynamic-inventory.py
+++ ansible/dynamic-inventory.py
@@ -6,7 +6,6 @@ import boto3
 
 aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
 aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
-# region_name = os.getenv('AWS_REGION')
 websrv_usr = os.getenv('WEBSRV_USR')
 
 ec2=boto3.resource("ec2")
@@ -16,24 +15,9 @@ bastion_instance = ec2.instances.filter(Filters=[{'Name': 'tag:server_type', 'Va
 for instance in bastion_instance:
 	if(instance.private_ip_address == None):
 		continue
-	#print(instance.private_ip_address)
 	bastion_host.append(instance.public_ip_address)
 
 bastion_host = bastion_host[0]
-#print(bastion_host)
-
-#inventory = {
-#        "all": {
-#	  "hosts": {
-#            "web_servers": [],
-#	    "proxy_servers":[],
-#		},
-#            "vars": {
-#                "ansible_user": f"{websrv_usr}",
-#                "ansible_ssh_common_args": f"-o StrictHostKeyChecking=no -J {websrv_usr}@{bastion_host}"
-#            }
-#        }
-#    }
 
 inventory = {
 	"web_servers": {
@@ -49,11 +33,19 @@ inventory = {
                 "ansible_user": f"{websrv_usr}",
                 "ansible_ssh_common_args": f"-o StrictHostKeyChecking=no -J {websrv_usr}@{bastion_host}"
             }
-	}
+	},
+	 "bastion_server": {
+        "hosts": [],
+        "vars": {
+                "ansible_user": f"{websrv_usr}",
+		"ansible_ssh_common_args": f"-o StrictHostKeyChecking=no"
+            }
+        }
 }
 
 web_server_instances = ec2.instances.filter(Filters=[{'Name': 'tag:server_type', 'Values': ['web_server']}])
 proxy_server_instances = ec2.instances.filter(Filters=[{'Name': 'tag:server_type', 'Values': ['proxy_server']}])
+bastion_server_instance = ec2.instances.filter(Filters=[{'Name': 'tag:server_type', 'Values': ['bastion_host']}])
 
 for instance in web_server_instances:
 	if(instance.private_ip_address == None):
@@ -65,23 +57,9 @@ for instance in proxy_server_instances:
 		continue
 	inventory["proxy_servers"]["hosts"].append(instance.private_ip_address)
 
-
-
-#inventory = {
-#    "web_servers": {
-#        "hosts": [
-#		"44#.55.3.226",
-#		"44.66.44.33"
-#		]
-#	},
-#	"proxy_servers": {
-#	"hosts": [
-#		"55.55.55.55",
-#		"33.33.33.33"
-#		]
-#   }
-#}
-
-#print(inventory["web_servers"]["hosts"][0])
+for instance in bastion_server_instance:
+        if(instance.public_ip_address == None):
+                continue
+        inventory["bastion_server"]["hosts"].append(instance.public_ip_address)
 
 print(json.dumps(inventory, indent=4, separators=(',', ': ')))
